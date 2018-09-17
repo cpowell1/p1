d0e09ef7b707b6194e0bd6efaa5f0fbf0dc96167
Christian Powell <christiant.powell@gmail.com>
2018-09-17 18:43:17 -0500

    Add image and info to index.php

--- index.php
+++ index.php
@@ -7,13 +7,18 @@
 		
 </head>
 <body>
-	
-	<h1>Christian Did It</h1>
+	<div class="container">
+		<h1>Christian Powell</h1>
+		
+		<img src="images/headshot.jpg" alt="headshot" title="Christian Powell" />
+		
+		<p>Hi, my name is Christian Powell, and I am a Web Strategy Manager in Nashville, TN. I also do freelance web design as a side gig. I went to University of Mississippi for an undergraduate degree in Marketing. I proceeded to go to the University of Florida where I got a Master of Business Administration and a Master of Art in Mass Communication in Web Design. I love to run, and I spend most of my free time with my dog.</p>
 
-	<?php 
-	echo 'Hello World! You are visiting ';
-	echo $_SERVER['SERVER_NAME']; 
-	?>
+		<?php 
+		echo 'Hello World! You are visiting ';
+		echo $_SERVER['SERVER_NAME']; 
+		?>
+	</div>
 	
 </body>
 </html>
