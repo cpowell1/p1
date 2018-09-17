455174e5b0245d74d26329716564902de6aba992
Christian Powell <christiant.powell@gmail.com>
2018-09-17 18:22:59 -0500

    Added index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Hello World</title>
+	<meta charset='utf-8'>
+		
+</head>
+<body>
+	
+	<h1>Christian Did It</h1>
+
+	<?php 
+	echo 'Hello World! You are visiting ';
+	echo $_SERVER['SERVER_NAME']; 
+	?>
+	
+</body>
+</html>
