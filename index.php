473b2870dc19993bd5f98caecb223b7db8e77902
Christian Powell <christiant.powell@gmail.com>
2018-09-17 19:35:23 -0500

    Create random quote string and style it

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
 		<img src="images/headshot.jpg" alt="headshot" title="Christian Powell" />
 		
 		<p>Hi, my name is Christian Powell, and I am a Web Strategy Manager in Nashville, TN. I also do freelance web design as a side gig. I went to University of Mississippi for an undergraduate degree in Marketing. I proceeded to go to the University of Florida where I got a Master of Business Administration and a Master of Art in Mass Communication in Web Design. I love to run, and I spend most of my free time with my dog.</p>
-
-		<?php 
-		echo 'Hello World! You are visiting ';
-		echo $_SERVER['SERVER_NAME']; 
-		?>
+		<div class="quotes">
+			<?php 
+			echo $quote[array_rand($quote)];
+			?>
+		</div>
 		
 	</div>
 	
---
+++ quotes-logic.php
@@ -0,0 +1,4 @@
+<?php 
+$quote = array('Be yourself; everyone else is already taken.','Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.',
+  'If you tell the truth, you don\'t have to remember anything.'); 
+?>
